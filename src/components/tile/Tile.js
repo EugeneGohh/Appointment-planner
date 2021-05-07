cc7e8c17549c9a94bab33ffbaff31196fffca5b2
Eugene Goh <gzkgt11@gmail.com>
2021-05-07 13:15:25 +0800

    Render data from object

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ tile }) => {
   return (
     <div className="tile-container">
-      
+      {Object.values(tile).map((value, i) => (
+        <p key={i} className={`${i === 0 ? "title-title" : ""} title`}>
+          {value}
+        </p>
+      ))}
     </div>
   );
 };
