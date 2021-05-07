ea1292c5e116f013282b8411c1979f01e62b74e1
Eugene Goh <gzkgt11@gmail.com>
2021-05-07 13:18:33 +0800

    Handle logic for adding new appointments

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({
+  appointments,
+  contacts,
+  addAppointment,
+}) => {
   /*
   Define state variables for 
   appointment info
   */
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [contact, setContact] = useState(
+    contacts.length > 0 ? contacts[0].name : ""
+  );
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+    addAppointment(title, contact, date, time);
+    setTitle("");
+    setDate("");
+    setTime("");
+    setContact("");
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          contacts={contacts}
+          contact={contact}
+          setContact={setContact}
+          title={title}
+          setTitle={setTitle}
+          time={time}
+          setTime={setTime}
+          date={date}
+          setDate={setDate}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList tiles={appointments} />
       </section>
     </div>
   );
