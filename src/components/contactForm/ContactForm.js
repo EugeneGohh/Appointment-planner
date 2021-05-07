63d52101f5069afa3682a6755eb8165b41b9b40e
Eugene Goh <gzkgt11@gmail.com>
2021-05-07 13:12:39 +0800

    Render form and add regex for MY number

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,47 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input 
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          placeholder="Contact Info"
+        />
+      </label>
+
+      <br />
+
+      <label>
+        <input 
+          type="tel"
+          value={phone}
+          name="phone"
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          pattern="^(\+?6?01)[0-46-9]-*[0-9]{7,8}$" // Regex for Malaysia phone number
+          placeholder="Enter Your Phone Number Here"
+        />
+      </label>
+
+      <br />
+
+      <label>
+        <input 
+          type="email"
+          value={email}
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="Your Email Here"
+        />
+      </label>
+
+      <br />
+      <input type="submit" value="Add Your Contact" />
+    </form>
   );
 };
