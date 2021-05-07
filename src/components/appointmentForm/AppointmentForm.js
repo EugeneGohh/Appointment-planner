57c87597b0bb2a904fb04394173eefe7f0efc396
Eugene Goh <gzkgt11@gmail.com>
2021-05-07 13:19:59 +0800

    Render appointment form

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 export const AppointmentForm = ({
   contacts,
@@ -10,7 +11,7 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
   const getTodayString = () => {
     const [month, day, year] = new Date()
@@ -19,7 +20,60 @@ export const AppointmentForm = ({
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
 
+  // Get contact name with this function
+  const getContact = () => {
+    return contacts.map((contact) => contact.name);
+  };
+
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          placeholder="What is your appointment?"
+        />
+      </label>
+
+      <br />
+
+      <label>
+        <ContactPicker
+          name="contact"
+          value={contact}
+          contacts={getContact()}
+          onChange={(e) => setContact(e.target.value)}
+          placeholder="Appointment contact"
+        />
+      </label>
+
+      <br />
+
+      <label>
+        <input
+          type="date"
+          name="date"
+          min={getTodayString()}
+          onChange={(e) => setDate(e.target.value)}
+          required
+          value={date}
+        />
+      </label>
+
+      <br />
+      <input
+        type="time"
+        name="time"
+        onChange={(e) => setTime(e.target.value)}
+        required
+        value={time}
+      />
+
+      <br />
+      <input type="submit" value="Add Your Appointment Here" />
+    </form>
   );
 };
