342c1c7d649e706b4c386256d9f57cd877dfc767
Eugene Goh <gzkgt11@gmail.com>
2021-05-07 13:20:14 +0800

    Render dropdown menu

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ name, contacts, onChange }) => {
   return (
-    ContactPicker
+    <select onChange={onChange} name={name}>
+      <option value="" key={-1} selected="selected">
+        No contact here
+      </option>
+      {contacts.map((contact) => {
+        return (
+          <option value={contact} key={contact}>
+            {contact}
+          </option>
+        );
+      })}
+    </select>
   );
 };
